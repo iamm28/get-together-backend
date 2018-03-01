216a71dacca96e5afdebc67787e3768b39cb8d1d
iamm28 <alisonmackay246@gmail.com>
2018-03-01 18:21:31 -0500

    adjusted serializer

--- app/serializers/users_serializer.rb
+++ app/serializers/users_serializer.rb
@@ -1,5 +1,5 @@
 class UsersSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :first_name, :last_name, :email, :age, :gender, :location, :interests
   # has_many :user_groups, dependent: :destroy
   # has_many :groups, through: :user_groups
   has_many :events, through: :groups
